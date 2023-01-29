4b8da8a4ee7fa658f005d583a61f2a47362db61f
leoier <leoier.feng@gmail.com>
2023-01-29 16:05:29 -0500

    minor fixes

--- src/visualize.py
+++ src/visualize.py
@@ -9,7 +9,8 @@ def draw_baseline():
     freqs = PitchFrequency().get_freq()
     major = [0, 2, 4, 5, 7, 9, 11]
     for i in range(len(freqs)):
-        plt.axline((0, freqs[i]), (1, freqs[i]), linewidth=1, linestyle = '-' if (i % 12) in major else '--')
+        plt.axline((0, freqs[i]), (1, freqs[i]), linewidth=1, linestyle = '-' \
+            if (i % 12) in major else '--')
 
 
 def draw_pitch(pitch):
@@ -46,12 +47,14 @@ def pitch_visualize(pitch, audio_file):
                                           frames=np.linspace(pitch.xmin, pitch.xmax, 500),
                                           interval=pitch.duration/500*1000)
 
-    # add the input soundtrack to 
+    # add the input soundtrack to output
     moving_axis.save('temp.mp4', writer='ffmpeg')
     input_video = 'temp.mp4'
     output_video = 'output.mp4'
 
-    command = ["ffmpeg", "-i", input_video, "-i", audio_file, "-c:v", "copy", "-c:a", "aac", "-strict", "experimental", output_video]
+    remix = ["ffmpeg", "-i", input_video, "-i", audio_file, \
+            "-c:v", "copy", "-c:a", "aac", "-strict", "experimental",\
+             output_video]
 
-    subprocess.run(command)
+    subprocess.run(remix)
     subprocess.run(["rm", "temp.mp4"])
